6b64925a73db2b5666ae05cbbcdb9632e00a8f8c
Alex Martinez <alex201899@gmail.com>
2022-03-17 00:49:43 -0500

    Updated Songs

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,37 +1,79 @@
 let songs = [
-{
-    artist: 'Voodoo Suite',
-    title: 'Little Grass Shack',
-    path: '../assets/01 Voodoo Suite - Little Grass Shack.mp3'
-},
-{
-    artist: 'Ken Hamm',
-    title: 'Buckbreak',
-    path: '../assets/02 Ken Hamm - Buckbreak.mp3'
-},
-{
-    artist: 'The Trumpeteers',
-    title: 'Litter Wooden Church',
-    path: '../assets/03 The Trumpeteers - Little Wooden Church.mp3'
-},
-{
-    artist: 'jackson F. Smith',
-    title: 'Cantina Rag',
-    path: '../assets/04 Jackson F. Smith - Cantina Rag.mp3'
-},
-{
-    artist: 'Studio Noir',
-    title: 'Our Little Hearts Like Saturn',
-    path: '../assets/05 Studio Noir - Our Little Hearts Like Saturn.mp3'
-},
-{
-    artist: 'Teddy adn Marge',
-    title: 'Dark Eyes',
-    path: '../assets/06 Teddy and Marge - Dark Eyes.mp3'
-},
-{
-    artist: 'Hr jothipala',
-    title: 'Ran Eta Pota Ridee Hawadi',
-    path: '../assets/07 HR Jothipala - Ran Eta Pota Ridee Hawadi.mp3'
-}
+    {
+        artist: 'Voodoo Suite',
+        title: 'Little Grass Shack',
+        path: '../assets/01 Voodoo Suite - Little Grass Shack.mp3'
+    },
+    {
+        artist: 'Ken Hamm',
+        title: 'Buckbreak',
+        path: '../assets/02 Ken Hamm - Buckbreak.mp3'
+    },
+    {
+        artist: 'The Trumpeteers',
+        title: 'Litter Wooden Church',
+        path: '../assets/03 The Trumpeteers - Little Wooden Church.mp3'
+    },
+    {
+        artist: 'jackson F. Smith',
+        title: 'Cantina Rag',
+        path: '../assets/04 Jackson F. Smith - Cantina Rag.mp3'
+    },
+    {
+        artist: 'Studio Noir',
+        title: 'Our Little Hearts Like Saturn',
+        path: '../assets/05 Studio Noir - Our Little Hearts Like Saturn.mp3'
+    },
+    {
+        artist: 'Teddy adn Marge',
+        title: 'Dark Eyes',
+        path: '../assets/06 Teddy and Marge - Dark Eyes.mp3'
+    },
+    {
+        artist: 'Hr jothipala',
+        title: 'Ran Eta Pota Ridee Hawadi',
+        path: '../assets/07 HR Jothipala - Ran Eta Pota Ridee Hawadi.mp3'
+    },
+    {
+        artist: 'The Anchorites',
+        title: 'Kingsfold',
+        path: '../assets/08 The Anchorites - Kingsfold(untrad.).mp3'
+    },
+    {
+        artist: 'Ludwigs Steirische Gaudi',
+        title: 'Bin hier Zuhause',
+        path: '../assets/09 Ludwigs Steirische Gaudi - Bin hier Zuhause (ID 02).mp3'
+    },
+    {
+        artist: 'lobo Loco',
+        title: 'Old River Boat',
+        path: '../assets/10 Lobo Loco - Old River Boat (ID 1368).mp3'
+    },
+    {
+        artist: 'Xylo-Ziko',
+        title: 'peril',
+        path: '../assets/11 Xylo-Ziko - peril.mp3'
+    },
+    {
+        artist: 'Crowander',
+        title: 'Humbug',
+        path: '../assets/12 Crowander - Humbug.mp3'
+    },
+    {
+        artist: 'marcos H. Bolanos',
+        title: 'Rain-Soaked Window',
+        path: '../assets/13 Marcos H. Bolanos - Rain-Soaked Window.mp3'
+    },
+    {
+        artist: 'Robert John',
+        title: 'Surface',
+        path: '../assets/14 Robert John - Surface.mp3'
+    },
+    {
+        artist: 'Monolog Rockstars',
+        title: 'At the Restaurant',
+        path: '../assets/15 Monolog Rockstars - At The Restaurant.mp3'
+    }
 ]
+
+export { songs };
