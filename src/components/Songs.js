27dd701fd686e64503540a3c64e9cb6bd77ffbee
Alex Martinez <alex201899@gmail.com>
2022-03-16 22:15:54 -0500

    Added some songs

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,5 +1,37 @@
-var Song = {
+let songs = [
+{
     artist: 'Voodoo Suite',
     title: 'Little Grass Shack',
     path: '../assets/01 Voodoo Suite - Little Grass Shack.mp3'
+},
+{
+    artist: 'Ken Hamm',
+    title: 'Buckbreak',
+    path: '../assets/02 Ken Hamm - Buckbreak.mp3'
+},
+{
+    artist: 'The Trumpeteers',
+    title: 'Litter Wooden Church',
+    path: '../assets/03 The Trumpeteers - Little Wooden Church.mp3'
+},
+{
+    artist: 'jackson F. Smith',
+    title: 'Cantina Rag',
+    path: '../assets/04 Jackson F. Smith - Cantina Rag.mp3'
+},
+{
+    artist: 'Studio Noir',
+    title: 'Our Little Hearts Like Saturn',
+    path: '../assets/05 Studio Noir - Our Little Hearts Like Saturn.mp3'
+},
+{
+    artist: 'Teddy adn Marge',
+    title: 'Dark Eyes',
+    path: '../assets/06 Teddy and Marge - Dark Eyes.mp3'
+},
+{
+    artist: 'Hr jothipala',
+    title: 'Ran Eta Pota Ridee Hawadi',
+    path: '../assets/07 HR Jothipala - Ran Eta Pota Ridee Hawadi.mp3'
 }
+]
