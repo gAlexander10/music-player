26965fb1c55604c9a25c6c95fe5bde61504cb15f
Alex Martinez <alex201899@gmail.com>
2022-03-19 01:50:24 -0500

    Updated styling

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,4 +1,5 @@
 import React,{ useState, useRef, useEffect} from 'react';
+import { TiMediaRewind, TiMediaFastForward } from 'react-icons/ti';
 import '../styles/audioplayer.css';
 import song from '../assets/Test.mp3';
 
@@ -7,14 +8,20 @@ function AudioPlayer() {
     <div>
       <div className="audioPlayerContainer">
         
+        <div className="abovePlayer">
+          <TiMediaRewind className="leftArrow"/>
+          <div className="currentSong">
+          Studio Noir - Our Little Hearts Like Saturn
+          </div>
+          <TiMediaFastForward className="rightArrow"/>
+        
         <div className="audioMiddle">
-          
           <audio controls>
             <source src={song} type="audio/mp3"/>
           </audio>
-
         </div>
-
+        
+        </div>
       </div>
     </div>
   );
