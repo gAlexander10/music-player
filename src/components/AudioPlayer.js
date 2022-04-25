95400afa69623a536ff188c239b5dc8ccccbe3ec
Alex Martinez <alex201899@gmail.com>
2022-04-24 20:25:13 -0500

    feat: useState

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -3,22 +3,27 @@ import { TiMediaRewind, TiMediaFastForward } from 'react-icons/ti';
 import '../styles/audioplayer.css';
 import song from '../assets/Test.mp3';
 import Playlist from '../data_structures/playlist';
-
-function Previous(){
-  // 1. Make the previous song the current song in the linked list -- Playlist.prev
-  //Playlist.prev();
-  // 2. Push previous Song onto Stack --  Push(id)
-  console.log('Previous Button Has been pushed');
-}
-
-function Next(){
-  // 1. Make the next song the current song in the linked list -- Playlist.next
-  //Playlist.next();
-  // 2. Push next Song onto Stack --  Push(id)
-  console.log('Next Button Has been pushed');
-}
+import songs from './Songs'
 
 function AudioPlayer() {
+ 
+  const [songName, setSongName] = useState('Song Name')
+  const [index, setIndex] = useState(0)
+  
+  const Previous = () => {
+    // 1. Make the previous song the current song in the linked list -- Playlist.prev
+    //Playlist.prev();
+    // 2. Push previous Song onto Stack --  Push(id)
+    console.log('Previous Button Has been pushed');
+  }
+
+  const Next = () =>{ 
+    // 1. Make the next song the current song in the linked list -- Playlist.next
+    //Playlist.next();
+    // 2. Push next Song onto Stack --  Push(id)
+    console.log('Next Button Has been pushed');
+  }
+
   return (
     <div>
       <div className="audioPlayerContainer">
@@ -28,7 +33,7 @@ function AudioPlayer() {
           <TiMediaRewind onClick={Previous} className="leftArrow"/>
 
           <div className="currentSong">
-          Studio Noir - Our Little Hearts Like Saturn
+          {songName}
           </div>
 
           <TiMediaFastForward onClick={Next} className="rightArrow"/>
