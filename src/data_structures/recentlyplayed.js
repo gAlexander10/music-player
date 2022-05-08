02528a60c1ef7beec7875adb4e59ff62be443d3b
CambryKing <cambryking@gmail.com>
2022-05-07 19:03:30 -0500

    Update

--- src/data_structures/recentlyplayed.js
+++ src/data_structures/recentlyplayed.js
@@ -6,10 +6,10 @@ class RecentlyPlayed {
 
     push(element){ //Adds new element to the array
         for(var i=0; i<15; i++){  //Checks if the contents of the array appear somewhere within the array
-            if(this.item[i] == element) //If a similar element is found, it is deleted
+            if(this.item[i] === element) //If a similar element is found, it is deleted
                this.item.splice(i, 1);
         }
-        if(this.item.length==15){ //If there are more than 15 elements, the oldest one is deleted
+        if(this.item.length===15){ //If there are more than 15 elements, the oldest one is deleted
             this.item.splice(0, 1);
         }
         this.item.push(element);
